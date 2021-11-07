f44a889f00b651af7dae63afdcddcc2e3f07c2f0
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-07 07:22:59 -0500

    attach Counter library functions. create mappings

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions provided by OpenZepplin.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+// inherirts from ERC721: standard NFT contract!
+contract MyEpicGame is ERC721 {
     // Define a new type(struct) with 6 fields
     struct CharacterAttributes {
         uint256 characterIndex;
@@ -14,9 +22,21 @@ contract MyEpicGame {
         uint256 attackDamage;
     }
 
+    // Attach library functions from Counters to Counters.Counter
+    // The tokenId is the NFTs unique identifier
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     // Create an array that will hold the default character data
     // Help for after minting when you want to know things like HP, AD, etc
     CharacterAttributes[] defaultCharacters;
+
+    // Create a mapping from the nft's tokenId => that NFTs attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // Create a mapping from an address => the NFTs tokenId.
+    // Stores the owner of the NFT so it can be refrenced later.
+    mapping(address => uint256) public nftHolders;
 
     // constructor that holds data to be passed into contract at initialization
     // These values get passed in from run.js
