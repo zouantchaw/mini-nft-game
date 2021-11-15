d34c345f751786ac0c3f60c6db10dd0ab68f5bff
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-15 07:44:10 -0500

    Update metadata game description

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -281,7 +281,7 @@ contract MyEpicGame is ERC721 {
                         charAttributes.name,
                         " -- NFT #: ",
                         Strings.toString(_tokenId),
-                        '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                        '", "description": "This is an NFT that lets people play in the game Angry Pep!", "image": "',
                         charAttributes.imageURI,
                         '", "attributes": [ { "trait_type": "Health Points", "value": ',
                         strHp,
