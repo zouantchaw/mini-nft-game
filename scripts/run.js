e7e07ed82506f8590c2b72614dd5ba46bed344ef
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-17 14:43:03 -0500

    uplaod media to ipfs

--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,14 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy(
-    ["Mendy", "Mbappe", "Salah"], // Names
-    ["https://i2-prod.football.london/chelsea-fc/news/article19156275.ece/ALTERNATES/s615/0_Edouard-Mendy.jpg", // Images
-    "https://static.straitstimes.com.sg/s3fs-public/styles/article_pictrure_780x520_/public/articles/2021/06/29/af_mbappe_2906.jpg?itok=gTAQjEbU&timestamp=1624949805", 
-    "https://img2.chinadaily.com.cn/images/201804/16/5ad3f9d6a3105cdce0a2e43a.jpeg"],
+    ["Haland", "Mbappe", "Salah"], // Names
+    ["QmZGp5n319GzGoqj8s5i57EWGni3119ZTBpbPVPrDHyNdQ", // Images
+    "QmUf7obvfandnYoCcoWs8ovSoFLGLJBYfSrRsbvYxKgkef", 
+    "QmRPPRngqV1bUb1r88SS5S6VcGpatSGwEB3ic1vBRr2vx5"],
     [200, 400, 600], // HP values
     [200, 100, 50], // Attack damage
     "Angry Pep", // Boss Name
-    "https://dynaimage.cdn.cnn.com/cnn/q_auto,h_600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F151026214217-22-what-a-shot-1027.jpg", // Boss Image
+    "ipfs://QmeuqxMiJpk3guV6fv4wzbRiQAadnNP5tDYuZkU3W4gZGz", // Boss Image
     10000, // Boss hp
     50 // Boss attack damage
   );
